f43fe685c77cde506e6bfdf0aad3f96c309af4e2
Josue David Hernandez Gutierrez <megadavidtor@gmail.com>
2023-07-19 10:34:11 -0600

    Disable github copilot by default
    Signed-off-by: Josue David Hernandez Gutierrez <megadavidtor@gmail.com>

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -2,6 +2,7 @@
 return {
   'github/copilot.vim',
   init = function()
+      vim.b.copilot_enabled = false
       vim.g.copilot_no_tab_map = true
       vim.api.nvim_set_keymap("i", "<C-A>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
   end,
