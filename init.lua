0b8d5b6eaa3b070252f2230ffab63b2476e9e9ca
Josue David Hernandez Gutierrez <me@josuedhg.com>
2026-06-04 17:34:50 -0600

    Fix(init.lua): Set treesitter tag
    Signed-off-by: Josue David Hernandez Gutierrez <me@josuedhg.com>

--- init.lua
+++ init.lua
@@ -900,6 +900,7 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    tag = 'v0.10.0',
     build = ':TSUpdate',
     -- main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
